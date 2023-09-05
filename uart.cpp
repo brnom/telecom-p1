5c2cc7db6c9baf7fde668f1dcc350412948e3a11
Bruno Mengaldo <brunomengaldo@gmail.com>
2023-09-05 17:38:15 -0300

    feat(uart): implement tests 1 - 4

--- uart.cpp
+++ uart.cpp
@@ -2,18 +2,108 @@
 
 void UART_RX::put_samples(const unsigned int *buffer, unsigned int n)
 {
-    // seu código aqui
+    int bit_num = 0;
+    FILE *dc = fopen("../dc.raw", "wb");
+
+    static enum class State {
+        IDLE,
+        SEARCHING,
+        RECEIVING
+    } state = State::IDLE;
+
+    for (int i = 0; i < n; i++)
+    {
+        int mod = i % (SAMPLES_PER_SYMBOL + 1);
+        int sample = buffer[i];
+        ring_buffer[mod] = sample;
+
+        switch (state)
+        {
+        case State::IDLE:
+            if (sample == 0)
+            {
+                state = State::SEARCHING;
+                bit_counter = 0;
+                bit_counter_noisy_tolerance = 0;
+            }
+            break;
+
+        case State::SEARCHING:
+            bit_counter++;
+
+            if (sample == 0)
+            {
+                if (bit_counter >= 30)
+                {
+                    state = State::RECEIVING;
+                    received_byte = 0;
+                    bit_counter_noisy_tolerance = 0;
+
+                    for (int j = (mod - (bit_counter % SAMPLES_PER_SYMBOL)); j != (mod - 1); j = (j + 1) % SAMPLES_PER_SYMBOL)
+                    {
+                        bit_num++;
+                        if (ring_buffer[j] == 1)
+                        {
+                            bit_counter_noisy_tolerance++;
+                        }
+                        else
+                        {
+                            bit_counter_noisy_tolerance = 0;
+                        }
+
+                        if (bit_counter_noisy_tolerance > 10)
+                        {
+                            i = i - (bit_num - bit_counter_noisy_tolerance);
+                            break;
+                        }
+                    }
+
+                    bit_counter_noisy_tolerance = 0;
+                }
+            }
+            else
+            {
+                bit_counter_noisy_tolerance++;
+
+                if (bit_counter_noisy_tolerance > 5)
+                {
+                    state = State::IDLE;
+                }
+            }
+            break;
+
+        case State::RECEIVING:
+            bit_counter++;
+
+            fwrite(&bit_counter, 1, sizeof(float), dc);
+
+            if ((bit_counter % SAMPLES_PER_SYMBOL - (SAMPLES_PER_SYMBOL / 2)) == 0)
+            {
+                received_byte |= (sample << (bit_counter / SAMPLES_PER_SYMBOL - 1));
+
+                if (bit_counter >= (9 * SAMPLES_PER_SYMBOL))
+                {
+                    state = State::IDLE;
+                    get_byte(received_byte);
+                }
+            }
+            break;
+        }
+    }
+
+    fclose(dc);
 }
 
 void UART_TX::put_byte(uint8_t byte)
 {
     samples_mutex.lock();
-    put_bit(0);  // start bit
-    for (int i = 0; i < 8; i++) {
+    put_bit(0);
+    for (int i = 0; i < 8; i++)
+    {
         put_bit(byte & 1);
         byte >>= 1;
     }
-    put_bit(1);  // stop bit
+    put_bit(1);
     samples_mutex.unlock();
 }
 
@@ -21,21 +111,23 @@ void UART_TX::get_samples(unsigned int *buffer, unsigned int n)
 {
     samples_mutex.lock();
     std::vector<unsigned int>::size_type i = 0;
-    while (!samples.empty() && i < n) {
+    while (!samples.empty() && i < n)
+    {
         buffer[i++] = samples.front();
         samples.pop_front();
     }
     samples_mutex.unlock();
 
-    while (i < n) {
-        // idle
+    while (i < n)
+    {
         buffer[i++] = 1;
     }
 }
 
 void UART_TX::put_bit(unsigned int bit)
 {
-    for (int i = 0; i < SAMPLES_PER_SYMBOL; i++) {
+    for (int i = 0; i < SAMPLES_PER_SYMBOL; i++)
+    {
         samples.push_back(bit);
     }
 }
--- uart.hpp
+++ uart.hpp
@@ -5,15 +5,22 @@
 #include <deque>
 #include <mutex>
 #include <stdint.h>
+#include <stdexcept>
 #include "config.hpp"
 
 class UART_RX
 {
 public:
-    UART_RX(std::function<void(uint8_t)> get_byte) :get_byte(get_byte) {}
+    UART_RX(std::function<void(uint8_t)> get_byte) : get_byte(get_byte) {}
     void put_samples(const unsigned int *buffer, unsigned int n);
+
 private:
     std::function<void(uint8_t)> get_byte;
+    float ring_buffer[SAMPLES_PER_SYMBOL + 1] = {};
+    unsigned int bit_counter = 0;
+    unsigned int bit_counter_noisy_tolerance = 0;
+    uint8_t received_byte = 0;
+    int count = 0;
 };
 
 class UART_TX
@@ -21,6 +28,7 @@ class UART_TX
 public:
     void put_byte(uint8_t byte);
     void get_samples(unsigned int *buffer, unsigned int n);
+
 private:
     std::deque<unsigned int> samples;
     std::mutex samples_mutex;
